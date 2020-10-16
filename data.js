e2505e0501f3a365f46168f4aec9183fe149f886
Rhone Lachner <fernandclay@gmail.com>
2020-10-16 12:58:50 -0700

    added forest pages and updated plan

---
+++ data.js
@@ -0,0 +1,21 @@
+// RADIO BUTTONS HERE?????
+export const mushroomData = [
+    {
+        id: 'pink mushroom',
+        image:'http://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+    },
+    {
+        id:'blue mushroom',
+        image:'http://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+    },
+    {   
+        id:'purple mushroom',
+        image:'http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+    },
+];
+export const gnomeData = [
+    {
+        id: 'gnome',
+        image:'http://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+    },
+];
---
+++ utils.js
@@ -0,0 +1,40 @@
+
+import { pokemonData } from './data.js';
+import { CART } from './constants.js';
+
+
+
+
+export function getLocalStorage() {
+
+    // go get localStorageTools from local storage and call them localStorageTools
+    let localStorageResults = JSON.parse(localStorage.getItem(CART));
+
+    // if there are no localStorageTools in local storage yet
+    // if they've never been to the site
+    if (!localStorageResults) {
+        // go grab the hard coded localStorageResults, and SEED local storage with them
+        const stringyCart = JSON.stringify(pokemonData);
+
+        localStorage.setItem(CART, stringyCart);
+        localStorageResults = pokemonData;
+    }
+
+    return localStorageResults;
+}
+
+
+export function getFromLocalStorage(key) {
+    const item = localStorage.getItem(key);
+    
+    return JSON.parse(item);
+}
+    
+    // setInLocalStorageFunction BEGINS HERE.
+export function setInLocalStorage(key, value) {
+    const stringyItem = JSON.stringify(value);
+    
+    localStorage.setItem(key, stringyItem);
+    
+    return value;
+}
